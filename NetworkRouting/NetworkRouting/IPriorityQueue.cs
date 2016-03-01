790905aca8540a79e0df8825601ffa86e0befb5d
David Taylor <David.Taylor@wavetronix.com>
2016-02-29 17:04:48 -0700

    added comments

--- NetworkRouting/NetworkRouting/IPriorityQueue.cs
+++ NetworkRouting/NetworkRouting/IPriorityQueue.cs
@@ -22,19 +22,31 @@ namespace NetworkRouting
         {
             queue = new List<double>(dist);
         }
+        //adds a distance to the queue.
+        //Insertion is O(1) for an array
+        public void insert(double dist)
+        {
+            queue.Add(dist);
+        }
+        //updates the element in queue at index with the given value
+        //DecreaseKey is O(1), because it is a simple access
         public override void decreaseKey(int index, double value)
         {
             queue[index] = value;
         }
-
+        //marks the minimum as done by replacing it with -1
+        //DeleteMin is O(|v|)
         public override int deletemin()
         {
+            //O(n) to find min
             double value = queue.Where(x => x != -1).Min();
+            //O(n) to set value
             int index = queue.IndexOf(value);
             queue[index] = -1;
             return index;
         }
-
+        //checks if the queue is empty
+        //O(|v|) to find if its empty
         public override bool isEmpty()
         {
             return queue.Where(x => x != -1).Count() == 0;
@@ -46,7 +58,8 @@ namespace NetworkRouting
         private List<double> distances = new List<double>();
         private List<int> pointers = new List<int>();
         private int lastIndex=Int32.MaxValue;
-
+        //initializes the distances with the given list and sets the lastIndex which
+        //is used to track which elements have been checked
         public PriorityQueueHeap(List<double> distances)
         {
             foreach(double distance in distances)
@@ -55,7 +68,8 @@ namespace NetworkRouting
             }
             lastIndex = distances.Count - 1;
         }
-
+        //adds element to the bottom of the tree and if its not the max value then bubble up
+        //Insert is O(log(|v|)) because Heapify is O(log(|v|)) and add is O(1)
         public void insert(double distance)
         {
             distances.Add(distance);
@@ -65,35 +79,42 @@ namespace NetworkRouting
                 HeapifyUp(distances.Count - 1);
             }
         }
-
+        //updates the distance at the index from the pointer list and 
+        //bubble up to make sure parents are smaller
+        //DecreaseKey is O(log(|v|)) because bubbleup is O(log(|v|))
         public override void decreaseKey(int index, double value)
         {
             distances[pointers.IndexOf(index)] = value;
             HeapifyUp(pointers.IndexOf(index));
         }
-
+        //swaps the minimum with the lastIndexed item and bubbles down
+        //DeleteMin is O(log(|v|)), because of the reorder
         public override int deletemin()
         {
             int node = pointers[0];
             pointers[0] = pointers[lastIndex];
             pointers[lastIndex] = node;
-
             distances[0] = distances[lastIndex];
             distances[lastIndex] = -1;
-
+            //this make it O(log(|v|))
             HeapifyDown(0);
             lastIndex--;
             return node;
         }
-
+        //checks if the queue is empty, if the initial element is -1 then that means 
+        //the queue is empty because all elements have been checked and set to -1
+        //O(1)
         public override bool isEmpty()
         {
             return distances[0] == -1;
         }
+        //bubble up to ensure that parents are smaller than the child
+        //BubbleUp is O(log(|v|)) because the index is divided by two each time we traverse
         private void HeapifyUp(int childIdx)
         {
             if (childIdx > 0)
             {
+                //this make it O(log(|v|))
                 int parentIdx = (childIdx - 1) / 2;
                 if (distances[childIdx] < distances[parentIdx])
                 {
@@ -105,24 +126,28 @@ namespace NetworkRouting
                     double nodedist = distances[parentIdx];
                     distances[parentIdx] = distances[childIdx];
                     distances[childIdx] = nodedist;
-
                     HeapifyUp(parentIdx);
                 }
             }
         }
-
+        //bubble down to ensure that children are larger than parent
+        //BubbleDown is O(log(|v|)) because we only traverse log(|v|) levels of the tree by multiplying 
+        //the index by 2
         private void HeapifyDown(int parentIdx)
         {
             if (distances[parentIdx] == -1)
                 return;
+            //this make it O(log(|v|))
             int leftChildIdx = 2 * parentIdx + 1;
             int rightChildIdx = leftChildIdx + 1;
             int largestChildIdx = parentIdx;
-            if (leftChildIdx < pointers.Count && distances[leftChildIdx] != -1 && distances[leftChildIdx] < distances[largestChildIdx])
+            if (leftChildIdx < pointers.Count && distances[leftChildIdx] != -1 && 
+                distances[leftChildIdx] < distances[largestChildIdx])
             {
                 largestChildIdx = leftChildIdx;
             }
-            if (rightChildIdx < pointers.Count && distances[rightChildIdx] != -1 && distances[rightChildIdx] < distances[largestChildIdx])
+            if (rightChildIdx < pointers.Count && distances[rightChildIdx] != -1 && 
+                distances[rightChildIdx] < distances[largestChildIdx])
             {
                 largestChildIdx = rightChildIdx;
             }
@@ -131,15 +156,14 @@ namespace NetworkRouting
                 int node = pointers[parentIdx];
                 pointers[parentIdx] = pointers[largestChildIdx];
                 pointers[largestChildIdx] =  node;
-
                 double nodedist = distances[parentIdx];
                 distances[parentIdx] = distances[largestChildIdx];
                 distances[largestChildIdx] = nodedist;
-
                 HeapifyDown(largestChildIdx);
             }
         }
     }
+    //factory used to make queue replaceable
     public class PriorityQueueFactory
     {
         public enum QueueType { Array, Heap}
--- NetworkRouting/NetworkRouting/PathCalculator.cs
+++ NetworkRouting/NetworkRouting/PathCalculator.cs
@@ -10,16 +10,20 @@ namespace NetworkRouting
 {
     public class PathCalculator
     {
-
-        public static void shortest(TextBox cost, TextBox arrayTimeBox, TextBox heapTimeBox, TextBox xspeedupBox,int startingIndex, List<PointF> points, List<HashSet<int>> connections, int stopIndex, Graphics graphics, bool compare)
+        //calculates the shorted path
+        public static void shortest(TextBox cost, TextBox arrayTimeBox, TextBox heapTimeBox, TextBox xspeedupBox,int startingIndex, List<PointF> points
+            ,List<HashSet<int>> connections, int stopIndex, Graphics graphics, bool compare)
         {
             DijkstraResult heapResult = dijkstra(startingIndex, points, connections, PriorityQueueFactory.QueueType.Heap);
+            //Grabbing the results of dijkstra
             List<int> prev = heapResult.getPrev();
             List<double> dist = heapResult.getDist();
             int currentIndex = stopIndex;
             bool isPath = true;
-            while(currentIndex != startingIndex)
+            //iterating through the result to make sure a complete path exists and to draw the path
+            while (currentIndex != startingIndex)
             {
+                //if the path doesn't exist then there is no reason to continue, and the cost is unreachable
                 if (currentIndex != startingIndex && prev[currentIndex] == -1)
                 {
                     isPath = false;
@@ -28,12 +32,13 @@ namespace NetworkRouting
                 PointF previousNode = points[prev[currentIndex]];
                 PointF currentNode = points[currentIndex];
                 graphics.DrawLine(Pens.Black, currentNode, previousNode);
-                double distance = dist[currentIndex] - dist[prev[currentIndex]];//distanceBetweenPoints(currentNode, previousNode);
+                double distance = dist[currentIndex] - dist[prev[currentIndex]];
                 graphics.DrawString(String.Format("{0}", (int)distance), SystemFonts.DefaultFont, Brushes.Black, midpoint(currentNode,previousNode));
                 currentIndex = prev[currentIndex];
             }
             if(isPath)
             {
+                //if the user wants to compare the array to the binary heap times we have to do dijkstras with the array
                 if (compare)
                 {
                     DijkstraResult arrayResult = dijkstra(startingIndex, points, connections, PriorityQueueFactory.QueueType.Array);
@@ -45,7 +50,7 @@ namespace NetworkRouting
             }
             else
             {
-                cost.Text = "No path";
+                cost.Text = "Unreachable";
             }
         }
 
@@ -54,28 +59,33 @@ namespace NetworkRouting
             return new PointF((point1.X + point2.X) / 2, (point1.Y + point2.Y) / 2);
         }
 
-
-        public static List<int> findShortestByHeap(List<PointF> points, List<HashSet<int>> connections)
-        {
-            return new List<int>();
-        }
-
         private static DijkstraResult dijkstra(int startingIndex, List<PointF> points, List<HashSet<int>> connections, PriorityQueueFactory.QueueType type)
         {
+            //start timer to see how long dijkstra's takes
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
             List<int> prev = new List<int>();
             List<double> dist = new List<double>();
+            //O(|v|)
             foreach (PointF point in points)
             {
+                //set all initial values to MaxValue for distances since we can't set it to infinity
                 dist.Add(Double.MaxValue);
+                //set all initial values to -1 for previous since we can't set it to infinity
                 prev.Add(-1);
             }
             dist[startingIndex] = 0;
+            //create the queue This factory allows for creating a queue, and initializes the queue with the distances.
+            //O depends on type of queue
             IPriorityQueue queue = PriorityQueueFactory.makeQueue(type,dist);
+            //while the queue is not empty we still have work to do
+            //O depends on type of queue
             while (!queue.isEmpty())
             {
+                //we need to pop the minimum off of the queue and process it.
+                //O depends on type of queue
                 int current = queue.deletemin();
+                //iterate through connections and store the distances if shorter than previously recorded
                 foreach (int index in connections[current])
                 {
                     double distance = dist[current] + distanceBetweenPoints(points[current], points[index]);
@@ -83,11 +93,14 @@ namespace NetworkRouting
                     {
                         dist[index] = distance;
                         prev[index] = current;
+                        //update the queue
+                        //O depends on type of queue
                         queue.decreaseKey(index, distance);
                     }
                 }
             }
             stopwatch.Stop();
+            //return the list of distances and previous, and the time it took
             return new DijkstraResult(prev, dist, stopwatch.Elapsed.TotalSeconds);
         }
         private static double distanceBetweenPoints(PointF point1, PointF point2)
